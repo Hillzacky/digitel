51e976d904a76643fffcc6d5c1ebe7f6f049d3a3
Hilmansyah <Hilm4nsyah@gmail.com>
2024-12-23 15:17:15 +0700

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,10 @@
+exports.commands = [
+    { command: '/menu', description: 'Main Menu' },
+    { command: '/deposit', description: 'Isi saldo. { nominal, bank, a/n }' },
+    { command: '/trx', description: 'Transaksi prabayar.' },
+    { command: '/cek', description: 'Periksa inquiry pascabayar.' },
+    { command: '/bayar', description: 'Bayar transaksi pascabayar.' },
+    { command: '/status', description: 'Tampilkan status pasca bayar' },
+    { command: '/ceksaldo', description: 'Periksa saldo.' },
+    { command: '/harga', description: 'Tampilkan daftar harga.' }
+];
