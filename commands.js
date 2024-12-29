e996a9b99b9051c7ee28d5736489c20aa9d0a730
Hilmansyah <Hilm4nsyah@gmail.com>
2024-12-29 14:46:41 +0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,7 @@
 exports.commands = [
     { command: '/menu', description: 'Main Menu' },
     { command: '/ceksaldo', description: 'Periksa saldo.' },
+    { command: '/help', description: 'Cara melakukan transaksi.' },
+    { command: '/harga', description: 'Tampilkan daftar harga terbaru.' },
     { command: '/ip', description: 'Tampilkan IP host.' }
 ];
