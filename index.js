4d89f805936c563b3c92419a96d587069f9ed963
Hilmansyah <Hilm4nsyah@gmail.com>
2024-12-29 22:37:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ bot.on('message', async (msg) => {
     break;
     case '/help':
       options = { parse_mode: 'markdown' }
-      resMsg = '**Cara Bertransaksi**\n\n**Transaksi Prepaid**\n```Rule  TRX code.tutuan.reportID```\n \n```CONTOH  TRX pulsatri50.08990666680.R#00125```\nUntuk pengecekan status kirim hal serupa dengan id report yang dikirim sebelumnya.\n\n**Transaksi PascaBayar**\n__Validasi id tujuan sebelum pembayaran__\n```Rule  CEK code.tujuan.reportID\n```__Pembayaran transaksi pasca__\n```Rule  BYR code.tujuan.reportID```\n__Cek status transaksi pascabayar__\n```Rule  STS code.tujuan.reportID```';
+      resMsg = '**Cara Deposit**\n-----\n```Rule  ISI nominal.namabank.atasnama\nISI 200000.MANDIRI.COPYSLAND```\n=====\n**Cara Bertransaksi**\n-----\n**Transaksi Prepaid**\n```Rule  TRX code.tujuan.reportID```\nJika tanpa reportID akan generate secara otomatis\n```CONTOH  TRX pulsatri50.08990666680.R#00125```\nUntuk pengecekan status kirim hal serupa dengan id report yang dikirim sebelumnya.\n\n**Transaksi PascaBayar**\n__Validasi id tujuan sebelum pembayaran__\n```Rule  CEK code.tujuan.reportID\n```__Pembayaran transaksi pasca__\n```Rule  BYR code.tujuan.reportID```\n__Cek status transaksi pascabayar__\n```Rule  STS code.tujuan.reportID```';
     break;
     case '/harga':
       options = Keyboard.make([
