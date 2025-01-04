9820056d68fb70b2c0840399880a6644743a9562
Hilmansyah <Hilm4nsyah@gmail.com>
2025-01-04 14:48:27 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -16,10 +16,10 @@ const objParse =(obj)=>{
   let str = '', key = '';
   for (const [k, v] of Object.entries(obj)) {
     key = k.replaceAll("_"," ");
-    if(typeof v.desc === 'object'){
+    if(typeof v.desc == 'object'){
       for (const [k2, v2] of Object.entries(v.desc)){
         str+=`\n`;
-        if(Array.isArray(v2.detail) || typeof v2.detail === 'object'){
+        if(Array.isArray(v2.detail) || typeof v2.detail == 'object'){
           for (i=0;i<k2.detail;i++){
             for (const [k3, v3] of Object.entries(v2.detail[i])){
               str+=`${k3}: ${v3}\n`;
